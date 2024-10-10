fbdd9b156675472a646348514825e574dd57956d
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-10-10 22:32:46 +0100

    Floor users registration

--- User/user.js
+++ User/user.js
@@ -1,23 +1,136 @@
-import mongoose from "mongoose"
+import { mongoose } from "mongoose"
+import {v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcrypt'
 
-export const userSchema = new mongoose.Schema({
-    _id: UUID,
-    userName: string,
-    email: string,
-    password: string
+ let userSchema = new mongoose.Schema({
+    _id: {type: String, default: uuidv4},
+    username: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
 })
 
-export const driverSchema = new mongoose.Schema({
-    _id: UUID,
-    userName: string,
-    email: string,
-    password: string,
-    plateNumber: UUID,
+ let driverSchema = new mongoose.Schema({
+    _id: {type: String, default: uuidv4},
+    username: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    plateNumber: {type: String, default: uuidv4, required: true},
 })
 
-export const adminSchema = new mongoose.Schema({
-    _id: UUID,
-    username: string,
-    email: string,
-    password: string,
+ let adminSchema = new mongoose.Schema({
+    _id: {type: String, default: uuidv4},
+    username: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
 })
+
+
+
+userSchema.pre('save', async function (next) {
+    let user = this;
+
+    if(!user.isModified('password')) {
+        return next();
+    }
+
+    try {
+        let salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        next();
+    } catch(err) {
+        next(err);
+    }
+})
+
+driverSchema.pre('save', async function (next) {
+    let user = this;
+
+    if(!user.isModified('password')) {
+        return next();
+    }
+
+    try {
+        let salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        next();
+    } catch(err) {
+        next(err);
+    }
+})
+
+adminSchema.pre('save', async function (next) {
+    let user = this;
+
+    if(!user.isModified('password')) {
+        return next();
+    }
+
+    try {
+        let salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        next();
+    } catch(err) {
+        next(err);
+    }
+})
+
+export let createUser = async (username, email, password, next) => {
+    let newUser = new User({
+        username: username,
+        email: email,
+        password: password,
+    });
+    await newUser.save();
+    console.log('User created with UUID:', newUser._id);
+   
+    
+};
+
+export let createAdmin = async (username, email, password) => {
+    let newAdmin = new Admin({
+        username: username,
+        email: email,
+        password: email,
+        plateNumber: plateNumber
+    });
+
+    await newAdmin.save();
+    console.log('User created with UUID:', newUser._id);
+    
+};
+
+export let createDriver = async (username, email, password, plateNumber) => {
+    let newDriver = new Driver({
+        username: username,
+        email: email,
+        password: email,
+        plateNumber: plateNumber
+    });
+
+    await newDriver.save();
+    console.log('User created with UUID:', newUser._id);
+    
+};
+
+export async function getUser() {
+    try {
+        const user = await User.find({});
+        return true
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+export async function getOneUser(email) {
+    const user = await User.findOne({email: email})
+    if (user) {
+        console.log(User.email)
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export let User = mongoose.model('User', userSchema);
+export let Admin = mongoose.model('Admin', adminSchema);
+export let Driver = mongoose.model('Driver', driverSchema);
--- app.js
+++ app.js
@@ -1,29 +1,111 @@
-import express from 'express';
+import express, { request } from 'express';
 import { mongoose } from 'mongoose';
+import { User, Admin, Driver, getUser, getOneUser, createAdmin, createDriver, createUser } from './User/user.js';
+import bcrypt from 'bcrypt';
+
 
 const app = express();
 
-const url = 'mongodb://192.168.56.3:27017';
+
+app.use(express.urlencoded({extended: true}))
 
 const dbName = 'movease';
+const url = `mongodb://192.168.56.3:27017/${dbName}`;
+mongoose.connect(url).then(() => console.log('Connected successfully')).catch((err) => console.log(err));
 
-mongoose.connect('url', {
-    userNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('Connected successfully')).catch((err) => console.log(err));
+
+
 
 app.get('/', (req, res) => {
     res.send(req.headers);
     res.send('Hello')
 });
 
+
+app.post('/login', (req, res) => {
+    if (User.findOne({emial: req.body.email}) && bcrypt.compare(User.password, req.body.password)) {
+        console.log('Welcome')
+    }
+})
+
 app.get('/login', (req, res) => {
-
+    res.send(` <form action="/login" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id='username' name='username'></input>
+        <label for="password">Password:</label>
+        <input type='password' id='password' name='password'></input>
+        <input type='submit' value='Login',/>
+    </form>
+    `)
+})
+app.post('/register', async (req, res) => { 
+    console.log( await getOneUser(req.body.email))  
+    if ( await getOneUser(req.body.email)){
+        res.send('Email already used. Create with a unique email')
+    } else {
+        createUser(req.body.username, req.body.email, req.body.password)
+        res.send('Created')
+    }
 });
 
-app.get('/register',(req, res) => {
-
+app.get('/register', (req, res) => {
+    
+    res.send(` <form action="/register" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id='username' name='username'></input>
+        <label for="email">Email:</label>
+        <input type="text" id='email' name='email'></input>
+        <label for="password">Password:</label>
+        <input type='password' id='password' name='password'></input>
+        <input type='submit' value='Register',/>
+    </form>
+    `)
+    
 });
+
+app.post('/register/driver',(req, res) => {
+    if (Driver.findOne({email: req.body.email})) {
+        res.send('Email exists, create an account with a unique email');
+    } else {
+        createDriver();
+    }
+});
+
+app.get('/register/driver', (req, res) => {
+    res.send(` <form action="/login" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id='username' name='username'></input>
+        <label for="plateNumber">Plate Number:</label>
+        <input type="text" id='plateNumber' name='plateNumber'></input>
+        <label for="email">Email:</label>
+        <input type="text" id='email' name='email'></input>
+        <label for="password">Password:</label>
+        <input type='password' id='password' name='password'></input>
+        <input type='submit' value='Login',/>
+    </form>
+    `)
+})
+
+app.post('/register/admin',(req, res) => {
+    if (Admin.findOne({email: req.body.email})) {
+        res.send('Email exists, create an account with a unique email');
+    } else {
+        createAdmin();
+    }
+});
+
+app.get('/register/admin', (req, res) => {
+    res.send(` <form action="/login" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id='username' name='username'></input>
+        <label for="email">Email:</label>
+        <input type="text" id='email' name='email'></input>
+        <label for="password">Password:</label>
+        <input type='password' id='password' name='password'></input>
+        <input type='submit' value='Login',/>
+    </form>
+    `)
+})
 
 app.get('/home', (req, res) => {
 
