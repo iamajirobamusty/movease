1558ebd670b5fe755610365ea86a044725ffe7e0
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2025-01-05 22:09:23 +0100

    Added dynamic mapping functionality using geolocation

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -7,8 +7,35 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //add a marker
-const marker = L.marker([6.5826, 3.3874]).addTo(map);
-marker.bindPopup('Ojota Lagos Nigeria').openPopup();
+const userMarker = L.marker([0, 0]).addTo(map);
+marker.bindPopup('Current location');
+
+function updateLocation(position) {
+    const { latitude, longitude } = position.coords;
+
+
+    //update the map view and marker position
+    map.setView([latitude, longitude], 15); //zoom to user locaatoin
+    userMarker.setLatLng([latitude, longitude]).openPopup();
+
+    //handle error in getting locatoin
+    function handleLocationError(error) {
+        console.error('Unale to get location', error.message);
+        alert('unable to retrieve location. Please enable location services.')
+    }
+
+    //request the user's location and update map dynamically
+    if (navigator.geolocation) {
+        //watch for location changes
+        navigator.geolocation.watchPosition(updateLocation, handleLocationError, {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+        });
+
+    } else {
+        alert("Geolocatoin is not supported by your browser")
+    }
+}
 
 //add a circle
 const circle = L.circle([51.508, -0.11], {
