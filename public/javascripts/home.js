bd364ee6535f4203c1b36c4b97efc2a551495c06
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2025-01-05 22:42:34 +0100

    Update dynamic user location

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,5 +1,5 @@
 //Initialize the map and set its view to a chosen geographical point
-const map = L.map('map').setView([6.5826, 3.3874], 15);
+const map = L.map('map').setView([0, 0], 2);
 
 //add a tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,41 +7,45 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //add a marker
-const userMarker = L.marker([0, 0]).addTo(map);
-marker.bindPopup('Current location');
+const userMarker = L.marker([0, 0]).addTo(map).bindPopup("Your location");
+
+// Add a circle (default position and size)
+    const userCircle = L.circle([0, 0], {
+        color: 'blue',
+        fillColor: '#3f8efc',
+        fillOpacity: 0.2,
+        radius: 100, // Default radius in meters
+    }).addTo(map);
 
 function updateLocation(position) {
-    const { latitude, longitude } = position.coords;
+    const { latitude, longitude, accuracy } = position.coords;
+
+    userMarker.setLatLng([latitude, longitude]).openPopup();
+    userCircle.setLatLng([latitude, longitude]);
+    userCircle.setRadius(accuracy);
 
 
     //update the map view and marker position
     map.setView([latitude, longitude], 15); //zoom to user locaatoin
-    userMarker.setLatLng([latitude, longitude]).openPopup();
-
-    //handle error in getting locatoin
-    function handleLocationError(error) {
-        console.error('Unale to get location', error.message);
-        alert('unable to retrieve location. Please enable location services.')
-    }
-
-    //request the user's location and update map dynamically
-    if (navigator.geolocation) {
-        //watch for location changes
-        navigator.geolocation.watchPosition(updateLocation, handleLocationError, {
-            enableHighAccuracy: true,
-            maximumAge: 0,
-        });
-
-    } else {
-        alert("Geolocatoin is not supported by your browser")
-    }
 }
 
-//add a circle
-const circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
-circle.bindPopup('Iam a circle')
+//handle error in getting locatoin
+function handleLocationError(error) {
+    console.error('Unale to get location', error.message);
+    alert('unable to retrieve location. Please enable location services.')
+}
+
+//request the user's location and update map dynamically
+if (navigator.geolocation) {
+    //watch for location changes
+    navigator.geolocation.watchPosition(updateLocation, handleLocationError, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+    });
+
+    
+} else {
+        alert("Geolocatoin is not supported by your browser")
+}
+
+
